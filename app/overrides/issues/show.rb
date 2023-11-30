27b376c4a0d878e0657f91bc3a39cdb62a77949d
Oleksii Nechytailo <alexeynechitailo@gmail.com>
2023-11-30 18:47:14 +0200

    Fix for zeitwerk loading mode

--- app/overrides/issues/show.rb
+++ app/overrides/issues/show.rb
@@ -1,5 +1,7 @@
-Deface::Override.new virtual_path: 'issues/_edit',
-                     name:         'add-timelog-issue-hook',
-                     insert_after: 'div.splitcontent',
-                     original:     '69fd1ee11dddc1b4fd8bf5d963bd6f342b273b6f',
-                     partial:      'issues/timelog_hook'
+module Issues::Show
+  Deface::Override.new virtual_path: 'issues/_edit',
+                       name:         'add-timelog-issue-hook',
+                       insert_after: 'div.splitcontent',
+                       original:     '69fd1ee11dddc1b4fd8bf5d963bd6f342b273b6f',
+                       partial:      'issues/timelog_hook'
+end
